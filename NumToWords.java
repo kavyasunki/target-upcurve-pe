83b1bdb5e4c074cb96dfd760ffa6011df808a906
kavyasunki <135207143+kavyasunki@users.noreply.github.com>
2024-03-23 15:58:56 +0530

    NumToWords.java

--- NumToWords.java
+++ NumToWords.java
@@ -33,7 +33,7 @@ public class NTW {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number");
+        System.out.print("Enter a number: ");
         int num = scanner.nextInt();
         scanner.close();
         System.out.println("In words: " + inWords(num));
