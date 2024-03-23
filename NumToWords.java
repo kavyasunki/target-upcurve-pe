6694409e75b19467962b25f5a3df11e7ede58e3e
kavyasunki <135207143+kavyasunki@users.noreply.github.com>
2024-03-23 15:58:01 +0530

    Add files via upload
    Number to Words

---
+++ NumToWords.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class NTW {
+    private static final String[] units = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+                                            "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    private static final String[] tens = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+    public static String inWords(int num) {
+        if (num == 0) {
+            return "zero";
+        }
+        return convertToWords(num);
+    }
+
+    private static String convertToWords(int num) {
+        if (num < 20) {
+            return units[num];
+        }
+        if (num < 100) {
+            return tens[num / 10] + ((num % 10 != 0) ? " " + units[num % 10] : "");
+        }
+        if (num < 1000) {
+            return units[num / 100] + " hundred" + ((num % 100 != 0) ? " " + convertToWords(num % 100) : "");
+        }
+        if (num < 100000) {
+            return convertToWords(num / 1000) + " thousand" + ((num % 1000 != 0) ? " " + convertToWords(num % 1000) : "");
+        }
+        if (num < 10000000) {
+            return convertToWords(num / 100000) + " lakh" + ((num % 100000 != 0) ? " " + convertToWords(num % 100000) : "");
+        }
+        return convertToWords(num / 10000000) + " crore" + ((num % 10000000 != 0) ? " " + convertToWords(num % 10000000) : "");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number");
+        int num = scanner.nextInt();
+        scanner.close();
+        System.out.println("In words: " + inWords(num));
+
+    }
+}
